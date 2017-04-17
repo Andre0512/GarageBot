7e2dfc8a8395a1f36be94d1daac92c63e3eb8c0c
Andre <andre.basche@gmail.com>
2017-04-17 19:30:42 +0200

    Config-Datei erstellt

--- garage.py
+++ garage.py
@@ -6,10 +6,18 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import time
 import RPi.GPIO as GPIO
+import yaml
+import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def getConfig():
+    with open(os.path.join(pwd, "./config.yml"), 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+    return cfg
 
 def start(bot, update):
         update.message.reply_text('Hi!')
@@ -25,7 +33,13 @@ def switchGarage(bot, update):
     GPIO.cleanup()
 
 def main():
-    updater = Updater("366736703:AAH4Cvc7-j4aQWro0sdlWvnh6MEgPA78nqY")
+    global pwd
+    global cfg
+
+    pwd = os.path.dirname(__file__)
+    cfg = getConfig()
+    
+    updater = Updater(cfg['bot']['token'])
 
     dp = updater.dispatcher
 
